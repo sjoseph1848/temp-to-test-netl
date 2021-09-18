69c7473240e3b547a17233380ff68ba7eda35d11
sjoseph1848 <sjoseph1848@gmail.com>
2021-09-18 14:14:30 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const fetchData = async () => {
+    const results = await axios.get('/.netlify/functions/helloWorld');
+    setMessage(results.data.message);
+    console.log(results.data.message);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h3>{message}</h3>
       </header>
     </div>
   );
